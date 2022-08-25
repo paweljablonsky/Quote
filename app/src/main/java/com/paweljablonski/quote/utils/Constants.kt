01bf032c8bd5db430e8106376cfa2e07edf2852a
paweljablonsky <70322641+paweljablonsky@users.noreply.github.com>
2022-08-25 18:20:25 +0200

    Update Constants.kt

--- app/src/main/java/com/paweljablonski/quote/utils/Constants.kt
+++ app/src/main/java/com/paweljablonski/quote/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.paweljablonski.quote.utils
 
 object Constants {
     const val BASE_URL = "https://quotes15.p.rapidapi.com/quotes/random/"
-    const val API_KEY = "?rapidapi-key=c65894d44cmsh8c7d8a1bba4c702p1e3349jsn2db2b71eb9a3"
+    const val API_KEY = "?rapidapi-key=someapikey"
 }
